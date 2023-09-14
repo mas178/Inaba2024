76e7c6c2c0ee6895466551369108fda367cae98e
mas178 <mas178jp@gmail.com>
2023-09-14 15:01:40 +0900

    VS model 1

---
+++ src/SimPlot.jl
@@ -0,0 +1,88 @@
+module SimPlot
+
+using DataFrames: DataFrame, SubDataFrame, nrow
+using Plots: Plot, plot!, plot, heatmap, twinx, PlotMeasures
+using StatsBase: mean
+
+function plot_output_df(df::DataFrame, skip::Int = 10)::Plot
+    p1 = plot(xl = "Generation", title = "Population")
+    plot!(df.generation, df.birth_rate, label = "Birth Rate")
+    plot!(df.generation, df.death_rate, label = "Death Rate")
+    plot!(twinx(), df.generation, df.N, label = "Population", line = :dash)
+
+    p2 = plot(xl = "Generation", title = "Cooperation")
+    plot!(df.cooperation_rate, label = "Cooperation Rate")
+    plot!(df.payoff_μ, label = "Payoff (μ)")
+
+    df = df[df.generation.%skip.==0, :]
+
+    p3 = plot(xl = "Generation", title = "Network Attributes")
+    plot!(df.generation, df.weight_μ, ribbon = df.weight_σ, fillalpha = 0.5, label = "Weight")
+    plot!(df.generation, df.L, label = "L")
+    plot!(df.generation, df.C, label = "C")
+    plot!(twinx(), df.generation, df.k, label = "<k>", line = :dash)
+
+    p4 = plot(xl = "Generation", title = "Component Attributes")
+    plot!(
+        df.generation,
+        df.component_size_μ,
+        ribbon = (df.component_size_μ - df.component_size_min, df.component_size_σ),
+        fillalpha = 0.5,
+        label = "Size",
+    )
+    plot!(df.generation, df.component_size_max, label = "Size (Max)")
+    plot!(twinx(), df.generation, df.component_count, label = "Count", line = :dash, yscale = :log10)
+
+    params1 = join(["$(k) = $(v)" for (k, v) in pairs(df[1, [2, 3, 10, 11]])], ", ")
+    params2 = join(["$(k) = $(v)" for (k, v) in pairs(df[1, 4:9])], ", ")
+
+    return plot(
+        p1,
+        p2,
+        p3,
+        p4,
+        layout = (2, 2),
+        size = (800, 800),
+        bottom_margin = 6 * PlotMeasures.mm,
+        suptitle = "$(params1)\n$(params2)",
+        plot_titlefontsize = 10,
+    )
+end
+
+function calc_cooperation_rate(df::DataFrame)::DataFrame
+    generations = nrow(df)
+    _df = DataFrame(df[1, 1:11])
+    _df.cooperation_rate .= mean(df.cooperation_rate[round(Int, generations * 0.1):end])
+
+    return _df
+end
+
+function make_cooperation_df(df_vec::Vector{DataFrame})::DataFrame
+    cooperation_df = vcat([calc_cooperation_rate(df) for df in df_vec]...)
+
+    return sort(cooperation_df, names(cooperation_df)[4:11])
+end
+
+function plot_cooperation_heatmap(df::SubDataFrame)::Plot
+    # using Makie
+    # using WGLMakie
+    # fig = Figure()
+    # ax = Axis(fig[1, 1], xlabel = "T", ylabel = "S")
+    # ax.limits = (0, 2, -1, 1)
+    # hm = WGLMakie.heatmap!(ax, df.T, df.S, df.cooperation_rate)  # , interpolate = true
+    # cbar = Colorbar(fig[1, 2], hm, label = "cooperation rate")
+    # return fig
+
+    title1 = join(["$(k) = $(v)" for (k, v) in pairs(df[1, 4:6])], ", ")
+    title2 = join(["$(k) = $(v)" for (k, v) in pairs(df[1, 7:11])], ", ")
+
+    df = sort(df, [:T, :S])[:, [:T, :S, :cooperation_rate]]
+    T = unique(df.T)
+    S = unique(df.S)
+
+    mat = reshape([df[df.T.==t.&&df.S.==s, :cooperation_rate][1] for s in S, t in T], length(S), length(T))
+
+    return heatmap(T, S, mat, xlabel = "T", ylabel = "S", title = "$(title1)\n$(title2)", titlefontsize = 9)
+end
+
+end  # end of module
---
+++ src/Simulation.jl
@@ -0,0 +1,349 @@
+module Simulation
+
+using DataFrames: DataFrame, nrow, dropmissing, select!, Not
+using Graphs: SimpleGraph, degree, gdistances, local_clustering_coefficient, connected_components
+using LinearAlgebra
+using Random: MersenneTwister
+using StatsBase: mean, std, sample, Weights
+
+export Model, Param, Strategy, C, D, interaction!, death_and_birth!, log!, run_all, plot_output_df
+
+@enum Strategy C D
+
+invert(s::Strategy)::Strategy = (s == C ? D : C)
+
+"""
+    ar1_model(β::Float64, σ::Float64, μ::Float64 = 1.0, T::Int = 100)::Vector{Float64}
+
+AR(1) Model to create environmental volatility (`env_severity_vec`).
+"""
+function ar1_model(
+    β::Float64,        # 自己回帰の係数。|β| < 1 の場合に平均回帰性を持つ。μ = α / (1 - β)
+    σ::Float64,        # std of white noise
+    T::Int,            # time steps
+    μ::Float64 = 1.0,  # expected average value
+)::Vector{Float64}
+    α = μ * (1 - β)    # 定数項。μから逆算する。
+    x = Vector{Float64}(undef, T + 1)
+    x[1] = μ
+
+    for t = 1:T
+        ϵ = σ * randn()  # white noise
+        x[t+1] = clamp(α + β * x[t] + ϵ, 0.0, 2.0)
+    end
+
+    return x
+end
+
+@kwdef struct Param
+    initial_N::Int = 1_000
+    T::Float64 = 1.1                         # Temptation payoff
+    S::Float64 = -0.1                        # Sucker's payoff
+    initial_graph_weight::Float64 = 0.5
+    interaction_freqency::Float64 = 1.0
+    relationship_volatility::Float64 = 0.1
+    birth_rate::Float64 = 0.1
+    δ::Float64 = 0.01                        # strength of selection
+    μ::Float64 = 0.0                         # mutation rate
+    β::Float64 = 0.1                         # environmental volatility (自己回帰の係数)
+    σ::Float64 = 0.1                         # environmental volatility (std of white noise)
+    generations::Int = 100                   # generations (Time steps)
+    rng::MersenneTwister = MersenneTwister() # random seed
+end
+
+mutable struct Model
+    N::Int                             # agents' population
+    strategy_vec::Vector{Strategy}     # agents' strategy
+    payoff_vec::Vector{Float64}        # agents' payoff
+    graph_weights::Matrix{Float16}     # agents' relationship
+    payoff_table::Dict{Tuple{Strategy,Strategy},Tuple{Float64,Float64}}
+    env_severity_vec::Vector{Float64}  # deathe rate = birth rate * env_severity_vec
+    param::Param
+
+    function Model(p::Param)
+        new(
+            p.initial_N,
+            fill(D, p.initial_N),
+            fill(0.0, p.initial_N),
+            (fill(1.0, (p.initial_N, p.initial_N)) - Matrix(I, p.initial_N, p.initial_N)) * p.initial_graph_weight,
+            Dict((C, C) => (1.0, 1.0), (C, D) => (p.S, p.T), (D, C) => (p.T, p.S), (D, D) => (0.0, 0.0)),
+            ar1_model(p.β, p.σ, round(Int, p.generations / 10)),
+            p,
+        )
+    end
+end
+
+"""
+    interaction!(model::Model)::Nothing
+
+1. Each agent becomes a focal agent.
+2. Each focal agent picks an opponent based on the edge weight.
+3. The agents then play a game and adjust their payoffs according to the payoff table.
+
+    |   |  C  |  D  |
+    |:-:|:---:|:---:|
+    | C |R = 1|  S  |
+    | D |  T  |P = 0|
+
+    - Snowdrift:          T > R > S > P
+    - Stag Hunt:          R > T > P > S
+    - Prisoner's Dilemma: T > R > P > S
+
+4. Following the game, the weight of the edge between them is updated:
+
+    - If C vs. C, the weight is increased by v (0.0 < v < 1.0).
+    - Otherwise, the weight is decreased by v.
+"""
+function interaction!(model::Model)::Nothing
+    # Reset payoff
+    model.payoff_vec .= 0.0
+
+    n_interaction = Int(round(model.N * model.param.interaction_freqency))
+    focal_id_vec = sample(model.param.rng, 1:model.N, n_interaction, replace = false)
+    all_ids = collect(1:model.N)
+    temp_weights = Vector{Float16}(undef, model.N - 1)
+
+    @inbounds for focal_id in focal_id_vec
+        # Pick an opponent
+        neighbors = deleteat!(copy(all_ids), focal_id)
+        @views temp_weights .= model.graph_weights[focal_id, neighbors]
+        opponent_id = sample(model.param.rng, neighbors, Weights(temp_weights))
+
+        strategy_pair = (model.strategy_vec[focal_id], model.strategy_vec[opponent_id])
+
+        # Update payoff
+        focal_payoff, opponent_payoff = model.payoff_table[strategy_pair]
+        model.payoff_vec[focal_id] += focal_payoff
+        model.payoff_vec[opponent_id] += opponent_payoff
+
+        # Update relationship
+        new_weight = if strategy_pair == (C, C)
+            min((1.0 + model.param.relationship_volatility) * model.graph_weights[focal_id, opponent_id], 1.0)
+        else
+            (1.0 - model.param.relationship_volatility) * model.graph_weights[focal_id, opponent_id]
+        end
+        model.graph_weights[focal_id, opponent_id] = new_weight
+        model.graph_weights[opponent_id, focal_id] = new_weight
+    end
+end
+
+classic_fitness(payoff::Float64, δ::Float64)::Float64 = 1.0 - δ + δ * payoff
+
+sigmoid_fitness(payoff::Float64, δ::Float64)::Float64 = 1.0 / (1.0 + exp(-δ * payoff))
+
+function pick_deaths(model::Model, n::Int)::Vector{Int}
+    neg_fitness_vec = sigmoid_fitness.(-model.payoff_vec, model.param.δ)
+    death_id_vec = []
+    try
+        death_id_vec = sample(model.param.rng, 1:model.N, Weights(neg_fitness_vec), n, replace = false)
+    catch e
+        @show model.N, n, size(neg_fitness_vec)
+        @show neg_fitness_vec
+        throw(e)
+    end
+
+    return sort(death_id_vec)
+end
+
+function pick_parents(model::Model, n::Int)::Vector{Int}
+    fitness_vec = sigmoid_fitness.(model.payoff_vec, model.param.δ)
+    parent_id_vec = sample(model.param.rng, 1:model.N, Weights(fitness_vec), n, replace = false)
+
+    return sort(parent_id_vec)
+end
+
+function normalize_graph_weights!(model::Model)::Nothing
+    initial_graph_weight_sum = model.N * (model.N - 1) * model.param.initial_graph_weight
+
+    # graph_weight_sum = sum(Float64.(model.graph_weights))
+    factor = 0.0
+    @inbounds @simd for weight in model.graph_weights
+        factor += Float64(weight)
+    end
+
+    # model.graph_weights ./= (graph_weight_sum / initial_graph_weight_sum)
+    factor = Float16(initial_graph_weight_sum / factor)
+    @inbounds @simd for i in eachindex(model.graph_weights)
+        model.graph_weights[i] *= factor
+    end
+
+    return
+end
+
+get_death_rate(model::Model, generation::Int)::Float64 =
+    model.env_severity_vec[ceil(Int, generation / 10)] * model.param.birth_rate
+
+"""
+    death_and_reproduction!(model::Model)::Tuple{Vector{Int},Vector{Int}}
+
+- Some agents die based on fitness.
+- Some agents reproduce based on fitness.
+- Child copies the parent's strategy.
+- Child copies the parent's relationship.
+- Strategy is mutated based on mutation rate.
+"""
+function death_and_birth!(model::Model, generation::Int)::Tuple{Vector{Int},Vector{Int}}
+    # number of deaths and births (should be calculated before model.N changed).
+    n_deaths = round(Int, model.N * get_death_rate(model, generation))
+    n_births = round(Int, model.N * model.param.birth_rate)
+
+    # death
+    if model.N - n_deaths > model.param.initial_N / 2
+        death_id_vec = pick_deaths(model, n_deaths)
+        survived_index = deleteat!(collect(1:model.N), death_id_vec)
+        deleteat!(model.strategy_vec, death_id_vec)
+        deleteat!(model.payoff_vec, death_id_vec)
+        model.N -= n_deaths
+
+        # model.graph_weights = model.graph_weights[survived_index, survived_index]
+        _new_weights = fill(Float16(0.0), model.N, model.N)
+        @inbounds for x = 1:model.N
+            @simd for y = 1:model.N
+                _new_weights[x, y] = model.graph_weights[survived_index[x], survived_index[y]]
+            end
+        end
+        model.graph_weights = _new_weights
+    else
+        death_id_vec = []
+    end
+
+    # birth
+    if model.N + n_births < model.param.initial_N * 2
+        parent_id_vec = pick_parents(model, n_births)
+        mutation_vec = rand(model.param.rng, n_births) .< model.param.μ
+
+        for i = 1:n_births
+            # strategy
+            parent_strategy = model.strategy_vec[parent_id_vec[i]]
+            parent_strategy = mutation_vec[i] ? invert(parent_strategy) : parent_strategy
+            push!(model.strategy_vec, parent_strategy)
+
+            # payoff
+            push!(model.payoff_vec, 0.0)
+        end
+
+        # relationship
+        # model.graph_weights = vcat(model.graph_weights, fill(Float16(0.0), (n_births, model.N)))
+        # model.N += n_births
+        # model.graph_weights = hcat(model.graph_weights, fill(Float16(0.0), (model.N, n_births)))
+        # model.graph_weights[(model.N-n_births+1):end, :] = model.graph_weights[parent_id_vec, :]
+        # model.graph_weights[:, (model.N-n_births+1):end] = model.graph_weights[:, parent_id_vec]
+        model.N += n_births
+        _new_weights = fill(Float16(0.0), (model.N, model.N))
+        @inbounds for x = 1:model.N-n_births
+            @simd for y = 1:model.N-n_births
+                _new_weights[x, y] = model.graph_weights[x, y]
+            end
+        end
+        @inbounds @simd for i = 1:n_births
+            _new_weights[model.N-n_births+i, :] = _new_weights[parent_id_vec[i], :]
+            _new_weights[:, model.N-n_births+i] = _new_weights[:, parent_id_vec[i]]
+            _new_weights[parent_id_vec[i], model.N-n_births+i] = 1.0
+            _new_weights[model.N-n_births+i, parent_id_vec[i]] = 1.0
+            _new_weights[model.N-n_births+i, model.N-n_births+i] = 0.0
+        end
+        model.graph_weights = _new_weights
+
+        normalize_graph_weights!(model)
+    else
+        parent_id_vec = []
+    end
+
+    return death_id_vec, parent_id_vec
+end
+
+function make_output_df(param::Param)::DataFrame
+    df = DataFrame([param])
+    select!(df, Not([:generations, :rng]))
+    df = repeat(df, param.generations)
+
+    df.generation .= Int16(0)
+    df.N .= Int16(0)
+    df.cooperation_rate .= Float16(0)
+    df.payoff_μ .= Float16(0)
+    df.death_rate .= Float16(0)
+    df.weight_μ .= Float16(0)
+    df.weight_σ .= Float16(0)
+    df.k .= Float16(0)
+    df.L .= Float16(0)
+    df.C .= Float16(0)
+    df.component_count .= Float16(0)
+    df.component_size_μ .= Float16(0)
+    df.component_size_max .= Float16(0)
+    df.component_size_min .= Float16(0)
+    df.component_size_σ .= Float16(0)
+
+    return df
+end
+
+unweighted_graph(graph_weights::Matrix, threshold::Float64)::SimpleGraph = SimpleGraph(graph_weights .> threshold)
+
+function log!(output::DataFrame, generation::Int, model::Model, skip::Int = 10)::Nothing
+    output[generation, 12:16] = [
+        generation,
+        model.N,                            # population
+        mean(model.strategy_vec .== C),     # 協力率
+        mean(model.payoff_vec),             # 平均ペイオフ
+        get_death_rate(model, generation),  # 死亡率
+    ]
+
+    (generation % skip != 0) && return
+
+    # 重みベクトル
+    weight_vec = [Float64(model.graph_weights[i, j]) for i = 1:model.N for j in 1:model.N if i < j]  # costly
+    weight_μ = mean(weight_vec)
+    weight_σ = std(weight_vec)
+
+    simple_g = unweighted_graph(model.graph_weights, 0.5)  # costly
+
+    # 平均次数 (<k>)
+    _k = mean(degree(simple_g))
+
+    # 平均距離 (L)
+    # _L_vec = collect(Iterators.flatten([gdistances(simple_g, i) for i = 1:model.N]))  # most costly
+    _L_vec = []
+    @inbounds @simd for i = 1:model.N
+        append!(_L_vec, gdistances(simple_g, i))
+    end
+    _L_vec = filter(x -> 0 < x <= model.N, _L_vec)
+    _L = length(_L_vec) > 0 ? mean(_L_vec) : 0.0
+
+    # 平均クラスタ係数 (C)
+    _C = mean(local_clustering_coefficient(simple_g, 1:model.N))  # costly
+
+    # コンポーネント
+    _components = [length(_component) for _component in connected_components(simple_g)]
+    std_components = std(_components)
+    isnan(std_components) && (std_components = 0.0)
+
+    output[generation, 17:end] = [
+        weight_μ,              # 17 重みの平均
+        weight_σ,              # 18 重みの標準偏差
+        _k,                    # 19 平均次数 <k>
+        _L,                    # 20 平均距離 (L)
+        _C,                    # 21 平均クラスタ係数 (C)
+        length(_components),   # 22 コンポーネント数
+        mean(_components) / model.N,     # 23 平均コンポーネントサイズ
+        maximum(_components) / model.N,  # 24 最大コンポーネントサイズ
+        minimum(_components) / model.N,  # 25 最小コンポーネントサイズ
+        std_components / model.N,        # 26 コンポーネントサイズの標準偏差
+    ]
+
+    return
+end
+
+function run(param::Param)::DataFrame
+    model = Model(param)
+    model.strategy_vec = rand(model.param.rng, [C, D], model.param.initial_N)
+    output_df = make_output_df(param)
+
+    for generation = 1:model.param.generations
+        interaction!(model)
+        death_and_birth!(model, generation)
+        log!(output_df, generation, model)
+    end
+
+    return output_df
+end
+
+end  # end of module
